44d519cafcd8e7fc249f65a16830b790bbbe106b
Raúl López Doña <rldona@gmail.com>
2021-06-23 12:48:22 +0200

    [FEAT] Created productMenu, productMenuItem and H1 componentes. Added mock file. Added images.

---
+++ src/mocks/index.tsx
@@ -0,0 +1,19 @@
+import {ProductMenu} from '../models';
+
+export const productMenuList: ProductMenu[] = [
+  {
+    title: 'Productos nuevos y destacados',
+  },
+  {
+    title: 'Zapatillas',
+  },
+  {
+    title: 'Ropa',
+  },
+  {
+    title: 'Ofertas',
+  },
+  {
+    title: 'Acceso para Members',
+  },
+];
