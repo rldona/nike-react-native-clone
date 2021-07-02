1041cecafd4fb6caf94e3b0b61522010e2fdb9bc
Raúl López Doña <rldona@gmail.com>
2021-07-02 14:34:55 +0200

    [FEAT] Config Search screen

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
-import {StyleSheet, Text, View} from 'react-native';
+import EvilIcons from 'react-native-vector-icons/Ionicons';
+
+import {Button} from '../../components/Button';
+import {H1} from '../../components/H1';
 
 export const SearchScreen = () => {
+  const [text, onChangeText] = useState('');
+
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>SearchScreen</Text>
+      <View style={styles.topContainer}>
+        <EvilIcons name="search" size={20} color="#000" />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Buscar producto"
+          autoFocus={true}
+        />
+        <TouchableOpacity activeOpacity={1} onPress={() => navigation.goBack()}>
+          <Text style={styles.cancelButton}>Cancelar</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView
+        style={styles.modalView}
+        contentContainerStyle={styles.searchContent}>
+        <Text>Search history list...</Text>
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonView}>
+          <Button size="small">
+            <Text>Cámara</Text>
+          </Button>
+        </View>
+        <View style={styles.buttonView}>
+          <Button size="small">
+            <Text>Fotos</Text>
+          </Button>
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,13 +58,69 @@ export const SearchScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
   },
-  text: {
-    textAlign: 'center',
-    fontSize: 30,
+  selectorWrapp: {
+    height: 60,
+    marginBottom: 2,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    paddingTop: 60,
+  },
+  topContainer: {
+    position: 'absolute',
+    top: 40,
+    left: 0,
+    width: '100%',
+    marginBottom: 20,
+    paddingLeft: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#EEE',
+    alignItems: 'center',
+    zIndex: 999,
+  },
+  input: {
+    height: 40,
+    width: '75%',
+    paddingLeft: 20,
+  },
+  cancelButton: {
+    color: '#999',
+    marginRight: 20,
+  },
+  searchContent: {
+    paddingTop: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 10,
+    left: 0,
+    marginTop: 20,
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderTopColor: '#EEE',
+  },
+  buttonView: {
+    width: '49%',
+  },
+  buttonClose: {
+    position: 'absolute',
+    top: 60,
+    right: 20,
+    width: '8%',
+    zIndex: 999,
   },
 });
