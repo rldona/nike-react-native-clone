8e15a4a8b46abff568c459dfd15adfd58e15454c
Raúl López Doña <rldona@gmail.com>
2021-07-12 17:13:40 +0200

    [FEAT] Added components

---
+++ src/components/ShoppingCartSummary/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const ShoppingCartSummary = () => {
+  return (
+    <View>
+      <Text style={styles.text}>ShoppingCartSummary</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#000',
+    fontSize: 30,
+    fontWeight: '400',
+    paddingLeft: 20,
+    paddingBottom: 15,
+  },
+});
