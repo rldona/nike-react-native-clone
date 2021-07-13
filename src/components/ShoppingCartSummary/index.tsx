b718e05034312850cd7b05001c44c471d765d057
Raúl López Doña <rldona@gmail.com>
2021-07-13 21:07:32 +0200

    [FEAT] Config resume shopping cart component

--- src/components/ShoppingCartSummary/index.tsx
+++ src/components/ShoppingCartSummary/index.tsx
@@ -4,17 +4,34 @@ import {StyleSheet, Text, View} from 'react-native';
 export const ShoppingCartSummary = () => {
   return (
     <View>
-      <Text style={styles.text}>ShoppingCartSummary</Text>
+      <View style={styles.row}>
+        <Text style={styles.subTotalText}>Subtotal</Text>
+        <Text style={styles.subTotalText}>89,99 €</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.subTotalText}>Envío</Text>
+        <Text style={styles.subTotalText}>Estandar - Gratis</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.totalText}>Total</Text>
+        <Text style={styles.totalText}>89,99 €</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    color: '#000',
-    fontSize: 30,
+  subTotalText: {
+    color: '#999',
+    fontSize: 16,
     fontWeight: '400',
-    paddingLeft: 20,
-    paddingBottom: 15,
+  },
+  totalText: {
+    color: '#000',
+    fontSize: 16,
+    fontWeight: '400',
+  },
+  row: {
+    justifyContent: 'space-around',
   },
 });
