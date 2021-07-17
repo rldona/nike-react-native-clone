a8d5cdcfce6002ed2124eaa8a22b2b14c47ce0f5
Raúl López Doña <rldona@gmail.com>
2021-07-17 23:58:10 +0200

    [FEAT] Added all components to shopping cart content

--- src/components/ShoppingCartContent/index.tsx
+++ src/components/ShoppingCartContent/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 
 import {useShoppingCart} from '../../hooks/useShoppingCart';
 
 import {H1} from '../H1';
 import {Button} from '../Button';
 import {EmptyResults} from '../EmptyResults';
+import {ShoppingCartPromoCode} from '../ShoppingCartPromoCode';
+import {ShoppingCartItem} from '../ShoppingCartItem/index';
+import {ShoppingCartSummary} from '../ShoppingCartSummary';
+import {Divider} from '../Divider';
 
 export const ShoppingCartContent = () => {
   const {shoppingCart, onPress} = useShoppingCart();
@@ -23,7 +27,15 @@ export const ShoppingCartContent = () => {
 
   return (
     <View style={styles.container}>
-      <H1>Cesta</H1>
+      <ScrollView>
+        <H1>Cesta</H1>
+        <ShoppingCartItem />
+        <Divider padding={10} />
+        <ShoppingCartPromoCode />
+        <Divider padding={10} />
+        <ShoppingCartSummary />
+      </ScrollView>
+
       <View style={styles.buttonContainer}>
         <Button onPress={onPress}>
           <Text>Pasar por caja</Text>
--- src/components/ShoppingCartPromoCode/index.tsx
+++ src/components/ShoppingCartPromoCode/index.tsx
@@ -3,18 +3,25 @@ import {StyleSheet, Text, View} from 'react-native';
 
 export const ShoppingCartPromoCode = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.text}>¿Tienes un código promocional?</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderTopWidth: 1,
+    borderTopColor: '#CCC',
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCC',
+  },
   text: {
     color: '#000',
-    fontSize: 30,
+    fontSize: 18,
     fontWeight: '400',
     paddingLeft: 20,
-    paddingBottom: 15,
   },
 });
